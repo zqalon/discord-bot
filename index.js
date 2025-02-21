8f9634b7c5f5aeb72a652d16644eb43e14c53b32
zqalon <89801024+zqalon@users.noreply.github.com>
2025-02-21 15:54:45 -0800

    started project

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!hello') {
+        message.reply('Hello there!');
+    }
+});
+
+client.login(process.env.BOT_TOKEN); // Replace with your bot token
