643f229f79ddc04ef2c1bf33308e0dce3b7f2e61
zqalon <89801024+zqalon@users.noreply.github.com>
2025-03-13 18:01:53 -0700

    fixed gif

--- commands/utility/gif.js
+++ commands/utility/gif.js
@@ -15,6 +15,20 @@ module.exports = {
 				)
 		),
 	async execute(interaction) {
-		await interaction.reply({ content: 'ping', ephemeral: true });
+		// Get the user's choice
+		const category = interaction.options.getString('category');
+
+		// Map categories to GIF URLs
+		const gifs = {
+			gif_funny: 'https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif',
+			gif_meme: 'https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif',
+			gif_movie: 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+		};
+
+		// Retrieve the correct GIF based on the user's choice
+		const gifUrl = gifs[category] || 'https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif';
+
+		// Send the GIF in response (visible only to the user)
+		await interaction.reply({ content: `Here's a ${category.replace('gif_', '')} GIF!`, files: [gifUrl], ephemeral: true });
 	},
 };
