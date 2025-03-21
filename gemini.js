bdea515743a87cb262fff7c9fe4e69dbdab6e0a8
zqalon <89801024+zqalon@users.noreply.github.com>
2025-03-21 16:09:58 -0700

    updated prompt

--- gemini.js
+++ gemini.js
@@ -8,7 +8,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const prompt = `The following is a conversation from a discord channel group chat. 
                 Create a simple, formatted schedule from the events the group chat members have planned. 
-                Prioritize simplicity. Use a table format that has a good design in Discord. Do not title the table. 
+                Prioritize simplicity. Use a table format that has a good design in Discord. Color code the table using standard unicode emojis. Do not title the table. Use users' @ when referring to them.
                 Disregard events that have already passed. If no future events are planned, show an empty schedule.\n\n`;
 
 let getSched = async function(logs){
